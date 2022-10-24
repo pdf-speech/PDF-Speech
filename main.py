71e44aee94cca63930844ad8eedfdace9f713240
Yash Chaudhary <ygcjal@gmail.com>
2022-10-25 03:15:33 +0530

    Super Final Commit

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from tkinter import filedialog
-import codecs
 import os
 from tkinter import *
 from gtts import gTTS
@@ -8,81 +6,68 @@ from translate import MyTranslate
 from pdf_to_txt import PDFToText
 
 root = Tk()
-root.geometry("1250x950")
-img = PhotoImage(file="assets/Images/bg.png")
+root.geometry("1250x600")
+img = PhotoImage(file="assets/Images/background.png")
 label = Label(root, image=img)
 label.place(x=0, y=0)
 root.title("TEXT TO SPEECH")
 
-Label(root, text="TEXT_TO_SPEECH", font="arial 20 bold", fg='gray', bg='#36454F').pack()
+Label(root, text="TEXT/PDF TO SPEECH", font="arial 20 bold", fg='black', bg='#fff').place(x=500, y=50)
 Msg = StringVar()
 lang = StringVar()
 
-label1 = Label(root, text="Enter Text", font='arial 15 bold', fg='gray', bg='#36454F').place(x=220, y=160)
-message_entry_field = Entry(root, textvariable=Msg, width=50, font="Robote 20")
+label1 = Label(root, text="Enter Text", font='arial 15 bold', fg='black', bg='#fff').place(x=220, y=160)
+message_entry_field = Text(root, width=50, font="Robote 20", wrap=WORD)
 message_entry_field.place(x=220, y=200, width=350, height=300)
 
-label2 = Label(root, text="Enter Language (hindi=hi marathi= mr english =en )", font='arial 15 bold', fg='gray', bg='#36454F').place(x=600, y=160)
-lang_entry_field = Entry(root, textvariable=lang, width=50)
-lang_entry_field.place(x=600, y=200)
-
-# translate_label = Label(root, text="Translate Label Test", font="arial 15 bold", fg="white", bg="#36454F").place(x=800, y=550)
-
-
-def browse_files():
-    filename = filedialog.askopenfilename(initialdir="C:/",
-                                          title="Nilay",
-                                          filetypes=(("Text files", "*.txt*"), ("all files", "*.*")))
-    return filename
+label2 = Label(root, text="Enter Language (Hindi='hi', Marathi='mr')", font='arial 15 bold', fg='black',
+               bg='#fff').place(x=600, y=160)
+lang_entry_field = Entry(root, textvariable=lang, width=50, font="Robote 20")
+lang_entry_field.place(x=600, y=200, width=350, height=40)
 
 
-def text_to_speech_file():
-    # language = lang_entry_field.get()
-    # with codecs.open(browse_files(), encoding='utf-8') as f:
-    #     file = f.read().replace("\n", " ")
+def text_to_speech(text, language):
+    speech = gTTS(text=text, lang=language, slow=False)
+    speech.save("assets/sounds/speech.mp3")
+    playsound('assets/sounds/speech.mp3', True)
+    os.remove('assets/sounds/speech.mp3')
+
+
+def translate(text_input, language):
+    translate_obj = MyTranslate(text_input=text_input, translate_to_language=language)
+    message = translate_obj.connection()
+    return message
+
+
+def file_handler():
     pdf_obj = PDFToText()
     pdf_data = pdf_obj.translate_pdf()
     language = lang_entry_field.get()
-    test = MyTranslate(text_input=pdf_data, translate_to_language=language)
-    translated_text = test.connection()
-    message = translated_text
-    speech = gTTS(text=message, lang=language, slow=False)
-    speech.save("sounds/speech.mp3")
-    playsound('assets/sounds/speech.mp3', True)
-    os.remove('assets/sounds/speech.mp3')
+    message = translate(text_input=pdf_data, language=language)
+    text_to_speech(text=message, language=language)
 
 
-def text_to_speech():
+def input_text_handler():
     language = lang_entry_field.get()
-    test = MyTranslate(text_input=message_entry_field.get(), translate_to_language=language)
-    translated_text = test.connection()
-    message = translated_text
-    speech = gTTS(text=message, lang=language, slow=False)
-    speech.save("sounds/speech.mp3")
-    playsound('assets/sounds/speech.mp3', True)
-    os.remove('assets/sounds/speech.mp3')
+    message = translate(text_input=message_entry_field.get("1.0", END), language=language)
+    text_to_speech(text=message, language=language)
 
 
-def exit():
+def exit_handler():
     root.destroy()
 
 
 def reset():
     Msg.set("")
     lang.set("")
-    # os.remove('speech.mp3')
-
-
-def stop():
-    os.stop('/speech.mp3')
 
 
 speak_button = PhotoImage(file="assets/Images/speak-man.png")
 Button(root, text="Speak", bg="white", compound=LEFT, image=speak_button, width=130, font="arial 14 bold",
-       command=text_to_speech).place(x=600, y=320)
+       command=input_text_handler).place(x=600, y=320)
 
 exit_button = PhotoImage(file="assets/Images/exit.png")
-Button(root, font='arial 15 bold', text='EXIT', compound=LEFT, image=exit_button, width=130, command=exit,
+Button(root, font='arial 15 bold', text='EXIT', compound=LEFT, image=exit_button, width=130, command=exit_handler,
        bg='white').place(x=750, y=320)
 
 reset_button = PhotoImage(file="assets/Images/reset.png")
@@ -90,10 +75,7 @@ Button(root, font='arial 15 bold', text='RESET', compound=LEFT, image=reset_butt
        bg='white').place(x=900, y=320)
 
 file_button = PhotoImage(file="assets/Images/file.png")
-Button(root, text=" Play PDF File", font='arial 15 bold', command=text_to_speech_file, compound=LEFT, image=file_button,
+Button(root, text=" Play PDF File", font='arial 15 bold', command=file_handler, compound=LEFT, image=file_button,
        width=200, bg='white').place(x=600, y=400)
 
-pause_file_button = PhotoImage(file="assets/Images/pause.png")
-Button(root, text="Pause File", font='arial 15 bold', command=stop, compound=LEFT, image=pause_file_button, width=180,
-       bg='white').place(x=850, y=400)
 root.mainloop()
--- translate.py
+++ translate.py
@@ -1,5 +1,5 @@
-import requests, uuid, json
-from main import *
+import requests
+import uuid
 
 # Add your key and endpoint
 key = "e080665c75bc4281819fa6c7b56330e9"
@@ -7,8 +7,6 @@ endpoint = "https://api.cognitive.microsofttranslator.com"
 
 
 class MyTranslate:
-    # location, also known as region.
-    # required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
     def __init__(self, text_input, translate_to_language):
         self.location = "centralindia"
         self.path = '/translate'
@@ -16,19 +14,17 @@ class MyTranslate:
         self.params = {
             'api-version': '3.0',
             'from': 'en',
-            'to': translate_to_language          # ['mr', 'hi']
+            'to': translate_to_language
         }
         self.constructed_url = endpoint + self.path
 
         self.headers = {
             'Ocp-Apim-Subscription-Key': key,
-            # location required if you're using a multi-service or regional (not global) resource.
             'Ocp-Apim-Subscription-Region': self.location,
             'Content-type': 'application/json',
             'X-ClientTraceId': str(uuid.uuid4())
         }
 
-        # You can pass more than one object in body.
         self.input_text = text_input
         self.body = [{
             'text': self.input_text
@@ -37,12 +33,6 @@ class MyTranslate:
     def connection(self):
         request = requests.post(self.constructed_url, params=self.params, headers=self.headers, json=self.body)
         response = request.json()
-        # print(response[0]["translations"][0]["text"])
         return response[0]["translations"][0]["text"]
 
-        # print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
 
-
-
-# test = Translate(text_input="Maza nav Yash aahe", translate_to_language="mr")
-# test.connection()
